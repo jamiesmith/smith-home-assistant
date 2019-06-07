99c71a6c314aadeec07e75107e626d7ca0c9fe12
Jamie Smith <jamie@nuclet.jamieandamy.com>
2019-06-07 09:14:08 -0400

    auth method changed

--- restart.sh
+++ restart.sh
@@ -6,13 +6,6 @@ function logit
     echo "$(date '+%Y-%m-%d %H:%M:%S') $*" >> /tmp/ha-restart-history.log
 }
 
-# JRS-TMP git pull --rebase
-# JRS-TMP if [ $? -ne 0 ]
-# JRS-TMP then
-# JRS-TMP     echo "Git pull failed"
-# JRS-TMP     exit 9
-# JRS-TMP fi
-
 if [ $(docker-compose ps homeassistant | grep -c Up) -gt 0 ]
 then
     logit "Up!"
