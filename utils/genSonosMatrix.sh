82f1d5b32ca14d05cfcdf54b89310b246cab5e51
Jamie Smith <github@jamieandamy.com>
2023-10-23 09:18:08 -0400

    missed a couple zwave name changes

--- utils/genSonosMatrix.sh
+++ utils/genSonosMatrix.sh
@@ -6,11 +6,12 @@ for add_to in $rooms
 do
 cat <<EOF
 drop_${add_to}:
-  alias: 'Drop ${add_to}  from group'
+  alias: Drop ${add_to} from group
   sequence:
-    - service: sonos.unjoin
-      entity_id:
-        - media_player.${add_to}
+  - service: media_player.unjoin
+    data: {}
+    target:
+      entity_id: media_player.${add_to}
 
 EOF
     for to_add in $rooms
@@ -21,12 +22,12 @@ cat <<EOF
 ${add_to}_add_${to_add}:
   alias: 'Add $to_add to $add_to'
   sequence:
-    - service: sonos.join
-      data:
-        master: media_player.${add_to}
-      entity_id:
-        - media_player.${to_add}
-
+  - service: media_player.join
+    data:
+      group_members:
+      - media_player.${to_add}
+    target:
+      entity_id: media_player.${add_to}
 EOF
 	    
 	fi
